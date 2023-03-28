d4fc28753214845fafae0283b1d086235c2d4e86
Nakisanzedaisy <nakisanzedaisy@gmail.com>
2023-03-28 12:27:08 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/_putchar.C
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * _putchar _writes the character c to stdout
+ * @C: The character to be printed
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int_putchar(char c)
+{
+	return(write(1, &c, 1));
+}	
