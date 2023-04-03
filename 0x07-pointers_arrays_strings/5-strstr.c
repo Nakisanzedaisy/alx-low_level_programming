527495f0fc21db75b5dd149c411c9cf770107bf8
Nakisaanzedaisy <nakisanzedaisy@gmail.com>
2023-04-03 15:09:22 -0700

    print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - Entry point
+ * @needle: input
+ * @haystack:input
+ * Return: Always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int haystack_len = strlen(haystack);
+	int needle_len = strlen(needle);
+	int j;
+
+	if (needle_len > haystack_len)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i <= haystack_len - needle_len; ++i)
+	{
+
+		for (j = 0; j < needle_len; ++j)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (j == needle_len)
+		{
+			return (&haystack[i]);
+		}
+
+	}
+}
